2d2ab7ec018f0f017a7d936b512107bdabc6f008
Jan Burger <jan@mashlab.co.za>
2019-07-12 10:17:53 +0200

    Updated comic books controller to use repository

--- src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
@@ -12,6 +12,7 @@ namespace ComicBookGallery.Data
                     {
                         new ComicBook()
                         {
+                            Id =1,
                             SeriesTitle = "The Amazing Spider-Man",
                             IssueNumber = 700,
                             DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
@@ -26,6 +27,7 @@ namespace ComicBookGallery.Data
                         },
                         new ComicBook()
                         {
+                            Id = 2,
                             SeriesTitle = "The Amazing Spider-Man",
                             IssueNumber = 657,
                             DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
@@ -41,23 +43,24 @@ namespace ComicBookGallery.Data
                         Favorite = false
 
 },
-            new ComicBook()
-            {
-                SeriesTitle = "Bone",
-                IssueNumber = 50,
-                DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
-                Artists = new Artist[]
+                        new ComicBook()
                         {
-                            new Artist() { Name = "Jeff Smith", Role = "Script" },
-                            new Artist() { Name = "Jeff Smith", Role = "Pencils" },
-                            new Artist() { Name = "Jeff Smith", Role = "Inks" },
-                            new Artist() { Name = "Jeff Smith", Role = "Letters" }
-                        },
-                Favorite = false
-            }
+                            Id = 3,
+                            SeriesTitle = "Bone",
+                            IssueNumber = 50,
+                            DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                            Artists = new Artist[]
+                                    {
+                                        new Artist() { Name = "Jeff Smith", Role = "Script" },
+                                        new Artist() { Name = "Jeff Smith", Role = "Pencils" },
+                                        new Artist() { Name = "Jeff Smith", Role = "Inks" },
+                                        new Artist() { Name = "Jeff Smith", Role = "Letters" }
+                                    },
+                            Favorite = false
+                        }
 
                     };
-        
+       
 
         public ComicBook GetComicBook(int id)
         {
